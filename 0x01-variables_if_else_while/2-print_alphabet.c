a04d2877e4e98cf4168ce50ad9258a75bc45e7cf
SafouaneCh <safouanecharki3@gmail.com>
2023-09-20 07:14:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabets in lower case
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
