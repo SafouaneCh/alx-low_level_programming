dbd15265c0a28fcb7d632b03d7088623dcbc27cd
SafouaneCh <safouanecharki3@gmail.com>
2023-09-20 06:57:18 -0700

    p or n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
  *
  * Description: positive or negative
  *
- * return: 0 (success)
+ * Return: 0 (success)
 */
 
 int main(void)
@@ -17,10 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive\n", n);
+		printf("%i is positive", n);
 	else if (n == 0)
-		printf("%i is zero\n", n);
+		printf("%i is zero", n);
 	else
-		printf("%i is negative\n", n);
+		printf("%i is negative", n);
 	return (0);
 }
