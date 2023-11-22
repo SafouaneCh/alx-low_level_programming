07e94280f171b89b57a4b321e21241211f1eac36
SafouaneCh <safouanecharki3@gmail.com>
2023-11-22 13:21:28 -0800

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	int sum = 0;
+
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; j < argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+
+		printf("%d\n", sum);
+	}
+}
