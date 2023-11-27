ce41c2e321715e382c0a21135fee61da16e9661b
SafouaneCh <safouanecharki3@gmail.com>
2023-11-27 07:54:41 -0800

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,8 +5,24 @@
  *
  * @str: string
  *
- * Return : 0
+ * Return: (0)
 */
 
 char *_strdup(char *str)
 {
+	char *array;
+	int i, r;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	array = malloc((i + 1) * sizeof(char));
+
+	for (r = 0; str[r]; r++)
+		array[r] = str[r];
+	return (array);
+}
