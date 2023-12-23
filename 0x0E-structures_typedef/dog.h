146d20eeda26919722b62ffa581123d48b82a205
SafouaneCh <safouanecharki3@gmail.com>
2023-12-23 01:55:33 -0800

    a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ *
+ * @name: dogs name
+ *
+ * @age: dogs age
+ *
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct in a bit kitty world
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
