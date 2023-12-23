23bd08e828cc01f798d85168bc8adde6c9f556c2
SafouaneCh <safouanecharki3@gmail.com>
2023-12-23 02:50:36 -0800

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_dog - prints a dog
+ *
+ * @d: the dog to prints
+ *
+ * Return : void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age ? d->age : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
