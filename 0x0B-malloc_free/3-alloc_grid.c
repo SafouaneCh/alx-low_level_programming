42d43f6b1656719a9cbeff38193def6a96f96ab6
SafouaneCh <safouanecharki3@gmail.com>
2023-12-23 07:14:55 -0800

    alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,3 +12,22 @@
 
 int **alloc_grid(int width, int height);
 {
+	if (whidth <= 0 || height <= 0){
+		return NULL;
+	}
+	int **grid = (int **)malloc(height * sizeof(int *));
+	
+	if (!grid){
+		return NULL;
+	}
+	for (int i = 0; i < height; i++){
+		grid[i] = (int *)calloc(width, sizeof(int));
+		if (!grid[i]){
+			for (int j = 0; j < i; j++){
+				free(grid[j]);
+			}
+			free(grid);
+		}
+	}
+	return (grid);
+}
