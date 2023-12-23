1ab14c1b8a0dc068c594a354daf787be340bac7c
SafouaneCh <safouanecharki3@gmail.com>
2023-12-23 02:14:21 -0800

    init dog

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * alloc_grid - retruns a pointer to a 2 dimensional array of integers
+ *
+ * @whith: the width
+ *
+ * @higth: the hight
+ *
+ * Return: 0 or 1
+*/
+
+int **alloc_grid(int width, int height);
+{
